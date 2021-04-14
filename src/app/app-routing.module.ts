67cf39b33335383222ba6af94856c934afc46410
Jithender <Jithender.Konduri@trigyn.com>
2021-04-14 18:07:11 +0530

    sample login registor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { RegistorComponent } from './_components/registor/registor.component';
+import { LoginComponent } from './_components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'registor', component: RegistorComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './_components/login/login.component';
+import { RegistorComponent } from './_components/registor/registor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistorComponent
   ],
   imports: [
     BrowserModule,
